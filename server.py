40e34bba04c01966a66f9f0da00d56605c83a5de
followhappyq <followhappyq@gmail.com>
2018-10-09 21:47:43 +0200

    'client'

--- server.py
+++ server.py
@@ -1,51 +1,55 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-clients = {}
-addresses = {}
 
-
-addr = ('localhost', 8686)
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(addr)
-
-
-def accept_connection():
+def accept_connections():
     while True:
-        client, clients_address = server.accept()
-        print(clients_address, ":has connected")
-        client.send(b'give your name', 'utf8')
-        addresses[client] = clients_address
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Hello! enter your nickname and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
 
 
-def handle_client(client): #takes client socket as argument
+def handle_client(client):
+
     name = client.recv(1024).decode("utf8")
-    message = "Hello, if you want quit, just write {quit}"
-    client.send(bytes(message, "utf8"))
-    msg = "{0} has joined the chat!".format(name)
-    send_message(bytes(msg, "utf8"))
+    welcome = 'Hi %s! if you want quit just write {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
     clients[client] = name
+
     while True:
         msg = client.recv(1024)
         if msg != bytes("{quit}", "utf8"):
-            send_message(msg, name + ": ")
+            broadcast(msg, name + ": ")
         else:
             client.send(bytes("{quit}", "utf8"))
             client.close()
             del clients[client]
-            send_message(bytes("%s has left the chat." % name, "utf8"))
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
             break
 
 
-def send_message(msg, prefix=""):  # prefix is for name identification.
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+def broadcast(msg, prefix=""):
 
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
+
+
+clients = {}
+addresses = {}
+
+ADDR = ('localhost', 33000)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
 
 if __name__ == "__main__":
-    server.listen(3)
+    SERVER.listen(5)
     print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_connection())
+    ACCEPT_THREAD = Thread(target=accept_connections)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
-    server.close()
+    SERVER.close()
